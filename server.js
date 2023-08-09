8ba799e15ecad9284677c896336d0384afcbe505
danielboe <danielboeber@gmail.com>
2023-08-09 13:59:40 +0200

    feat(main) add vlc integration basics WIP feat(deploy) vlc install feat(main) raspberry pi switch on init

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const util = require("util");
 var path = require("path");
 var psTree = require("ps-tree");
 const Gpio = require("onoff").Gpio;
-const { exec } = require("child_process");
+const { exec, spawn } = require("child_process");
 var fs = require("fs");
-const AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
-
 const Parser = require("./parser.js");
+
+const env = Object.create(process.env);
+env.DISPLAY = ":0";
+const AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
+var DEBUG = false;
 
 const Buttons = [];
 var State = {};
@@ -14,6 +17,8 @@ var BlockButton = false;
 var StopMainFunction = false;
 var PlayerTask = null;
 var Volume = 500;
+var RPI = false;
+var vlcPlayerTask;
 
 var killall = function (pid, signal, callback) {
   signal = signal || "SIGKILL";
@@ -88,6 +93,42 @@ async function OmxPlayFile(file, volume = Volume) {
     } else {
       console.log("Button still blocked");
     }
+  });
+}
+
+async function vlcPlayer(file, loop = false, audio = false, volume = Volume) {
+  var fileName = file;
+  playerParams = ["-f", "--no-osd", "--control", "dbus"];
+  if (loop) {
+    playerParams.push("--loop");
+  }
+  if (!audio) {
+    playerParams.push("--no-audio");
+  }
+  playerParams.push(fileName);
+
+  return new Promise(async (resolve, reject) => {
+    console.log("Launching player");
+    var playerTask = spawn("cvlc", ["-f", "--no-osd", "--no-audio", "--control", "dbus", fileName], { env: env });
+
+    playerTask.stdout.on("data", (data) => {
+      console.error(`stdout: ${data}`);
+    });
+
+    playerTask.stderr.on("data", (data) => {
+      if (DEBUG) console.error(`stderr: ${data}`);
+    });
+
+    playerTask.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+    resolve(playerTask);
+  });
+}
+
+async function VlcPlayFile(file, volume = Volume) {
+  return new Promise(async (resolve, reject) => {
+    vlcPlayerTask = await vlcPlayer(file);
   });
 }
 
@@ -196,18 +237,25 @@ Parser.init({ configpath: "./media/", configfile: "config_files.json" }).then(fu
   Parser.parseConfig().then((Config) => {
     //console.log("By ID XX " + Parser.getFileById(23));
     MainFunction();
-    for (var i = 0; i < Config.trigger.length; i++) {
-      if (Config.trigger[i].gpio != undefined) {
-        console.log("----------NEW BUTTON GPIO: " + Config.trigger[i].gpio + "--------");
-        attachButton(Config.trigger[i]);
+    if (RPI) {
+      for (var i = 0; i < Config.trigger.length; i++) {
+        if (Config.trigger[i].gpio != undefined) {
+          console.log("----------NEW BUTTON GPIO: " + Config.trigger[i].gpio + "--------");
+          attachButton(Config.trigger[i]);
+        }
       }
     }
 
     console.log("----------INIT DONE-----------------");
+    VlcPlayFile("media/testvid.mp4");
   });
 });
 
 process.on("SIGINT", (_) => {
   //led.unexport();
   //button1.unexport();
+  if (vlcPlayerTask != undefined) {
+    vlcPlayerTask.kill();
+  }
+  process.exit();
 });
